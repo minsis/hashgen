c16348e6902f54fa7dd9b9e02f18a3341aa29eaf
Dennis Whitney <denniswhitney@gmail.com>
2016-01-30 13:15:37 -0600

    fixed syntax issues, removed print statements from all functions

--- hashgen.py
+++ hashgen.py
@@ -6,13 +6,15 @@ avial_hash = ["sha384", "sha224", "md5", "sha512", "sha1", "sha256"]
 avial_hash.sort()
 dict_hash = dict()
 default_hash = "md5"
+use_hash = str()
+file_path = str()
 
 print("List of available hash algorithms")
 for k, v in enumerate(avial_hash, start=1):
     print("({}) {}".format(k,v))
 
 while use_hash not in avial_hash:
-    use_hash = input("Select an algorithm to use: [%s]") %(default_hash)
+    use_hash = input("Select an algorithm to use[{}]".format(default_hash))
     if use_hash == "": use_hash = default_hash; break
 
 while os.path.isfile(file_path) is False:
@@ -22,6 +24,6 @@ while os.path.isfile(file_path) is False:
 avial_hash = convert_to_dict(avial_hash)
 
 start_time = time.time()
-hash_value = avial_hash[use_hash](file_path)
-print("Your {} hash value is: {}".format(use_hash,hash_value)
-print("Execution Time: %s seconds" %(time.time() - start_time))
+hash_value = md5(file_path) #avial_hash[use_hash](file_path)
+print("Your {} hash value is: {}".format(use_hash,hash_value))
+print("Execution Time: {} seconds".format(time.time() - start_time))
--- libs/hashinglib.py
+++ libs/hashinglib.py
@@ -1,6 +1,6 @@
 import hashlib
 
-def md5hash(file_path):
+def md5(file_path):
     hashed_file = hashlib.md5()
     with open(file_path, "rb") as open_file:
         read_byte = open_file.read(1048576)
@@ -10,10 +10,9 @@ def md5hash(file_path):
             read_byte = open_file.read(1048576)
 
     md5_hash = hashed_file.hexdigest()
-    print("md5 hash: " + md5_hash)
     return md5_hash
 
-def sha1hash(file_path):
+def sha1(file_path):
     hashed_file = hashlib.sha1()
     with open(file_path, "rb") as open_file:
         read_byte = open_file.read(1048576)
@@ -23,10 +22,9 @@ def sha1hash(file_path):
             read_byte = open_file.read(1048576)
 
     sha1_hash = hashed_file.hexdigest()
-    print("sha1 hash: " + sha1_hash)
     return sha1_hash
 
-def sha224hash(file_path):
+def sha224(file_path):
     hashed_file = hashlib.sha224()
     with open(file_path, "rb") as open_file:
         read_byte = open_file.read(1048576)
@@ -36,10 +34,9 @@ def sha224hash(file_path):
             read_byte = open_file.read(1048576)
 
     sha224_hash = hashed_file.hexdigest()
-    print("sha224 hash: " + sha224_hash)
     return sha224_hash
 
-def sha256hash(file_path):
+def sha256(file_path):
     hashed_file = hashlib.sha256()
     with open(file_path, "rb") as open_file:
         read_byte = open_file.read(1048576)
@@ -49,10 +46,9 @@ def sha256hash(file_path):
             read_byte = open_file.read(1048576)
 
     sha256_hash = hashed_file.hexdigest()
-    print("sha256 hash: " + sha256_hash)
     return sha256_hash
 
-def sha384hash(file_path):
+def sha384(file_path):
     hashed_file = hashlib.sha384()
     with open(file_path, "rb") as open_file:
         read_byte = open_file.read(1048576)
@@ -62,10 +58,9 @@ def sha384hash(file_path):
             read_byte = open_file.read(1048576)
 
     sha384_hash = hashed_file.hexdigest()
-    print("sha384 hash: " + sha384_hash)
     return sha384_hash
 
-def sha512hash(file_path):
+def sha512(file_path):
     hashed_file = hashlib.sha512()
     with open(file_path, "rb") as open_file:
         read_byte = open_file.read(1048576)
@@ -75,7 +70,6 @@ def sha512hash(file_path):
             read_byte = open_file.read(1048576)
 
     sha512_hash = hashed_file.hexdigest()
-    print("sha512 hash: " + sha512_hash)
     return sha512_hash
 
 def convert_to_dict(hash_list):
@@ -85,4 +79,4 @@ def convert_to_dict(hash_list):
     return hash_dict
 
 
-def compare(hash):
+#def compare(hash):
